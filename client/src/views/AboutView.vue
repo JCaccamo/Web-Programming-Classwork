ed842c17cb64d4825ab28aeb27b2921d6829efac
JCaccamo <caccamojason@gmail.com>
2022-10-06 16:15:28 -0400

    Remove HelloWorld.vue, update Nav.vue and AboutView.vue

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,15 +1,61 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+  const items = [
+    { name: 'Shoes', cost: 30},
+    { name: 'Shirt', cost: 30},
+    { name: 'Pants', cost: 30},
+  ]
+
+  const cart = reactive([
+    { name: 'Shoes', cost: 30, quantity: 1 },
+    { name: 'Shirt', cost: 30, quantity: 1 },
+    { name: 'Pants', cost: 30, quantity: 1 },
+  ])
+
+  function addToCart(item: any) {
+    cart.push({ ...item, quantity: 1 })
+  }
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="title">This is an about page</h1>
+    <ul>
+      <li v-for="item in items" :key="item.name" @click="addToCart(item)">
+        <h3>{{ item.name }}</h3>
+        <em>{{ item.cost }}</em>
+      </li>
+    </ul>
+    <h3 class="title is-3">Cart</h3>
+    <table class="table is-bordered">
+      <tr>
+        <th>Items</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Cost</th>
+      </tr>
+      <tr v-for="item in cart" :key="item.name">
+        <td>{{ item.name }}</td>
+        <td>${{ item.cost }}</td>
+        <td><input v-model="item.quantity"></td>
+        <td>${{ item.cost * item.quantity }}</td>
+      </tr>
+      <tr>
+        <td colspan="3">Total</td>
+        <td>${{ cart.reduce((total, item) => total + item.cost * item.quantity, 0) }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
+<style lang="scss">
+  ul {
     display: flex;
-    align-items: center;
+    li {
+      width: 200px;
+      padding: 5px;
+      margin: 5px;
+    }
   }
-}
 </style>
