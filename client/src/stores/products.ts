17c30b3b82dc24cc9360a36c45b4a5054c434025
JCaccamo <caccamojason@gmail.com>
2022-11-22 00:35:59 -0500

    Added error handling, removed counter.ts

--- client/src/stores/products.ts
+++ client/src/stores/products.ts
@@ -1,34 +1,30 @@
-import myFetch from "@/services/myFetch";
+import { api } from "./session";
 
 export function getProducts() {
-  return myFetch<ListEnvelope<Product>>('products');
+  return api<ListEnvelope<Product>>('products');
 }
 
 export function getProduct(id: number) {
-  return myFetch<Product>(`products/${id}`)
-}
-
-export function deleteProduct(id: number) {
-  data.products = data.products.filter( (product) => product.id !== id );
+  return api<Product>(`products/${id}`)
 }
 
 export interface ListEnvelope<T> {
-    products: T[]
-    total: number
-    skip: number
-    limit: number
-  }
-  
-  export interface Product {
-    id: number
-    title: string
-    description: string
-    price: number
-    discountPercentage: number
-    rating: number
-    stock: number
-    brand: string
-    category: string
-    thumbnail: string
-    images: string[]
-  }
+  products: T[]
+  total: number
+  skip: number
+  limit: number
+}
+
+export interface Product {
+  id: number
+  title: string
+  description: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  brand: string
+  category: string
+  thumbnail: string
+  images: string[]
+}
