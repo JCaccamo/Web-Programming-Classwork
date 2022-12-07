ef4a07587b2079ac9b7f28762658f3b9af1ffc35
JCaccamo <caccamojason@gmail.com>
2022-12-07 00:13:11 -0500

    Google APIs

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,6 +1,26 @@
 <script setup lang="ts">
     import { RouterLink } from 'vue-router';
     import session, { login, logout } from '../stores/session'
+    async function google_login() {
+        const auth_client = google.accounts.oauth2.initTokenClient({
+          client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+          scope: 'profile email ',           
+          callback: async function(token:any) {               
+            console.log({ token });
+            const data = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+              headers: { Authorization: `Bearer ${token.access_token}` },
+            });
+            const user = await data.json();
+            console.log({ user });
+            
+          },
+        });
+        auth_client.requestAccessToken();
+    }
+</script>
+
+<script lang="ts">
+    declare const google: any;
 </script>
 
 <template>
@@ -8,7 +28,7 @@
         <a class="button is-primary">
             Sign up
         </a>
-        <RouterLink class="button is-light" to="login">
+        <RouterLink class="button is-light" to="login" @click.prevent="google_login">
             Log in
         </RouterLink>
     </div>
