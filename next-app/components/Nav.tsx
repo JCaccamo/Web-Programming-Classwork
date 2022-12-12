191b6447af29948d666d1dd14701593a36a1a331
JCaccamo <caccamojason@gmail.com>
2022-12-12 14:33:26 -0500

    Update Nav.tsx using hooks

--- next-app/components/Nav.tsx
+++ next-app/components/Nav.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link"
+import { useState } from "react"
 const Cart = () => <div></div>
 const LoginBadge = () => <div></div>
 const MessageList = () => <div></div>
 
 export default () => {
+    const [isActive, setIsActive] = useState(false);
+    // const isCartOpen = ref(false);
     return (
         <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
             <Cart is-open="isCartOpen" />
@@ -12,13 +15,13 @@ export default () => {
                     <div className="navbar-item">
                         <h1 className="title is-5">Web Programming</h1>
                     </div>
-                    <a className="navbar-burger { 'is-active': isActive }" click="isActive = !isActive" role="button" aria-label="menu" aria-expanded="false">
+                    <a className={`navbar-burger ${ isActive ? 'is-active': '' }`} onClick={()=> setIsActive( !isActive )} role="button" aria-label="menu" aria-expanded="false">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                     </a>
                 </div>
-                <div id="navbarBasicExample" className="navbar-menu { 'is-active': isActive }">
+                <div id="navbarBasicExample" className={`navbar-menu ${ isActive ? 'is-active': '' }`}>
                     <div className="navbar-start">
                         <Link href="/" className="navbar-item">Home</Link>
                         <Link href="/products" className="navbar-item">Products</Link>
